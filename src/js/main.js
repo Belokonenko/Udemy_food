e28e64903b0670b9c9d4a513377126d0fefd88a9
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2022-10-29 08:40:39 +0300

    added timer

--- src/js/main.js
+++ src/js/main.js
@@ -38,5 +38,52 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
     //========/tabs============
+    
+    
+    //========date ============
+    const deadline = "2023-05-11";
+    
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((t / (1000 * 60 * 60)) % 24); 
+        const minutes = Math.floor((t / 1000 / 60) % 60);
+        const seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+
+    }; 
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days  = timer.querySelector('#days');
+        const hours = timer.querySelector('#hours');
+        const minutes = timer.querySelector('#minutes');
+        const seconds = timer.querySelector('#seconds');
+        const timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHtml = t.days;
+            hours.innerHtml = t.hours;
+            minutes.innerHtml = t.minutes;
+            seconds.innerHtml = t.seconds;
+
+            // if(t.total <= 0) {
+            //     clearInterval(timeInterval);
+            // };
+        };
+    };
+
+    setClock('.timer', deadline);
+    //========/date============
 });
+
 
